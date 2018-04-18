f8b0048e71b6a66a242bbbcdb45e0608bbf1669b
John Gruber <john.gruber@gitbit.org>
2018-04-18 16:22:08 -0400

    removed twiends because it's too agressive apparently

--- packages/bot/twiends.js
+++ packages/bot/twiends.js
@@ -1,18 +1,18 @@
 const {start} = require('../twiends/index')
 const {twitterAccount} = require('../data')
-const {csv, commonPassword} = require('../data')
+// const {csv, commonPassword} = require('../data')
 
 process.on('unhandledRejection', r => console.log(r))
 
 const s = async () => {
   await start(twitterAccount.username, twitterAccount.password)
 
-  const people = await csv()
-  for (let i = 0; i < people.length; i++) {
-    if (people[i].twitter) {
-      await start(people[i].twitter, commonPassword)
-    }
-  }
+  // const people = await csv()
+  // for (let i = 0; i < people.length; i++) {
+  //   if (people[i].twitter) {
+  //     await start(people[i].twitter, commonPassword)
+  //   }
+  // }
 }
 
 s()
