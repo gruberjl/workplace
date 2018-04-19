ec79ad55bd4706dc3a0e17ea582485165475e074
John Gruber <john.gruber@gitbit.org>
2018-04-19 10:10:10 -0400

    updated browse to be more natural flowing

--- packages/twitter/lib/browse.js
+++ packages/twitter/lib/browse.js
@@ -1,4 +1,4 @@
-/* eslint no-empty: 0 */
+/* eslint no-empty: 0, no-unused-vars: 0 */
 const Driver = require('../../driver')
 const {By, Key} = Driver.webdriver
 
@@ -38,22 +38,72 @@ const findAndLikeTweet = async (driver, tweet) => {
   }
 }
 
+const browseMoments = async (driver) => {
+  const url = 'https://twitter.com/i/moments'
+
+  await driver.get(url)
+  await driver.sleep(1000)
+
+  const body = driver.findElement(By.tagName('body'))
+  const timesToPageDown = Math.floor(Math.random() * 11)
+  for (let i = 0; i < timesToPageDown; i++) {
+    await body.sendKeys(Key.PAGE_DOWN)
+    await driver.sleep(Math.floor(Math.random()*(2000-500+1)+500))
+  }
+}
+
+const browseNotifications = async (driver) => {
+  const url = 'https://twitter.com/i/notifications'
+  await driver.get(url)
+  await driver.sleep(Math.floor(Math.random()*(5000-500+1)+500))
+}
+
+const browseMessages = async (driver) => {
+  try {
+    const messageBtn = await driver.findElement(By.className('global-dm-nav'))
+    await driver.executeScript('arguments[0].scrollIntoView(true);', messageBtn)
+    await driver.sleep(500)
+    await messageBtn.click()
+    await driver.sleep(Math.floor(Math.random()*(2000-1000+1)+1000))
+    const markAllReadBtn = await driver.findElement(By.className('mark-all-read'))
+    await markAllReadBtn.click()
+    const body = driver.findElement(By.tagName('body'))
+    await body.sendKeys(Key.ESCAPE)
+  } catch (e) {
+
+  }
+}
+
+const browseFeed = async (driver) => {
+  const body = driver.findElement(By.tagName('body'))
+  const timesToPageDown = Math.floor(Math.random()*(25-5+1)+5)
+  for (let i = 0; i < timesToPageDown; i++) {
+    await body.sendKeys(Key.PAGE_DOWN)
+    await driver.sleep(Math.floor(Math.random()*(2000-500+1)+500))
+  }
+}
+
 
 const browse = async (username, password) => {
   const driver = await login(username, password)
 
-  const tweets = await driver.findElements(By.className('tweet'))
+  // const tweets = await driver.findElements(By.className('tweet'))
+  //
+  // for (let i = 0; i < tweets.length; i++) {
+  //   let tweetText
+  //   try {
+  //     tweetText = await tweets[i].findElement(By.className('tweet-text'))
+  //   } catch (e) { }
+  //
+  //   if (tweetText) {
+  //     await findAndLikeTweet(driver, tweets[i])
+  //   }
+  // }
+  await browseFeed(driver)
+  await browseMoments(driver)
+  await browseNotifications(driver)
+  await browseMessages(driver)
 
-  for (let i = 0; i < tweets.length; i++) {
-    let tweetText
-    try {
-      tweetText = await tweets[i].findElement(By.className('tweet-text'))
-    } catch (e) { }
-
-    if (tweetText) {
-      await findAndLikeTweet(driver, tweets[i])
-    }
-  }
   return driver
 }
 
