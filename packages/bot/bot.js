cf5edc47705c0c81b047b00d36e5e25957d5a509
John Gruber <john.gruber@gitbit.org>
2018-05-19 16:47:40 -0400

    updated twitter-post to run mon-fri at 6:00,12:00,4:00pm

--- packages/bot/bot.js
+++ packages/bot/bot.js
@@ -1,23 +1,29 @@
 const { fork } = require('child_process')
-// const schedule = require('node-schedule')
-const chalk = require('chalk')
+const schedule = require('node-schedule')
+process.env.DEBUG ='*'
 
 const startTwitterPost = () => {
+  const debug = require('debug')('twitterPost')
+  debug('Starting twitter post')
   const twitterPost = fork('twitter-post')
 
   twitterPost.on('message', (msg) => {
-    console.log('Message from child', msg)
+    if (typeof msg == 'string')
+      debug(`Message: ${msg}`)
   })
 
   twitterPost.on('close', (code) => {
-    console.log(`child process exited with code ${code}`)
+    debug(`twitter post complete with code ${code}`)
   })
 }
 
 const start = () => {
-  console.log('Starting twitterPost')
-  startTwitterPost()
+  const rule = new schedule.RecurrenceRule()
+  rule.dayOfWeek = [new schedule.Range(1, 5)]
+  rule.hour = [6, 12, 16]
+  rule.minute = 0
 
+  schedule.scheduleJob(rule, startTwitterPost)
 }
 
 start()
--- packages/bot/twitter-post.js
+++ packages/bot/twitter-post.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const {Posts, People} = require('../sharepoint')
+const twitter = require('../twitter')
+
+const start = async () => {
+  const posts = await Posts.get('$filter=approved eq 1 and postedat eq null and app eq \'twitter\'&$orderby=postafter&$top=1')
+
+  if (posts.length>0 & new Date(posts[0].postafter) < new Date()) {
+    await postToTwitter(posts[0].summary)
+    Posts.update(posts[0].ID, {postedat: (new Date().toISOString())})
+  } else {
+    process.send('No tweets are ready for twitter.')
+  }
+}
+
+const postToTwitter = async (summary) => {
+  process.send(`tweeting: ${summary}`)
+  const person = (await People.get('ID eq 20'))[0]
+  const result = await twitter.post(person.twitter_access_token, person.twitter_access_token_secret, summary)
+  result.data.id ? process.send('successfully posted.') : process.send('failed to post')
+}
+
+start()
